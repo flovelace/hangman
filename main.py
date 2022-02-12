7b414bbfb981a5cac7d77f9bebe58dbce66b68ea
fairfax <lovelace.af@gmail.com>
2022-02-12 00:52:58 -0500

    added visuals, and fixed the bugs, so the game works now!

--- main.py
+++ main.py
@@ -1,35 +1,59 @@
 import random
 from words import words
+from hangman_visual import lives_visual_dict
 import string
 
 def get_valid_word(words):
-    word = random.choice(words) # randomly chooses a word from the list
-    while '-' in word or ' ' in word: #the whole loop will run until it reaches a word that does not have those characters
+    word = random.choice(words)  # randomly chooses something from the list
+    while '-' in word or ' ' in word:
         word = random.choice(words)
-    
+
     return word.upper()
+
 
 def hangman():
     word = get_valid_word(words)
-    word_letters = set(word) # saves letters in the word that have been guessed
+    word_letters = set(word)  # letters in the word
     alphabet = set(string.ascii_uppercase)
-    used_letters = set() # empty variable to keep track of what letters the user has guessed
+    used_letters = set()  # what the user has guessed
 
-    while len(word_letters) > 0:
-        print('You have used these letters: ', ' '.join(used_letters))
-        # what the current word is (ie W - R D)
+    lives = 7
+
+    # getting user input
+    while len(word_letters) > 0 and lives > 0:
+        # letters used
+        # ' '.join(['a', 'b', 'cd']) --> 'a b cd'
+        print('You have', lives, 'lives left and you have used these letters: ', ' '.join(used_letters))
+
+        # what current word is (ie W - R D)
         word_list = [letter if letter in used_letters else '-' for letter in word]
+        print(lives_visual_dict[lives])
         print('Current word: ', ' '.join(word_list))
 
-    user_letter = input('Guess a letter: ').upper() # gets the user input
-    if user_letter in alphabet - used_letters:
-        used_letters.add(user_letter)
-        if user_letter in word_letters:
-            word_letters.remove(user_letter)
-    
-    elif user_letter in used_letters:
-        print('You have already used this letter! Please try again!')
+        user_letter = input('Guess a letter: ').upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+                print('')
 
+            else:
+                lives = lives - 1  # takes away a life if wrong
+                print('\nYour letter,', user_letter, 'is not in the word.')
+
+        elif user_letter in used_letters:
+            print('\nYou have already used that letter. Guess another letter.')
+
+        else:
+            print('\nThat is not a valid letter.')
+
+    # gets here when len(word_letters) == 0 OR when lives == 0
+    if lives == 0:
+        print(lives_visual_dict[lives])
+        print('You died, sorry. The word was', word)
     else:
-        print('Invalid character. Please try again.')
+        print('YAY! You guessed the word', word, '!!')
 
+
+if __name__ == '__main__':
+    hangman()
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Hangman
+## Purpose
+The purpose of this game is to learn about storing information in variables and using if/elif loops.
+## Built with
+Python3
+## Github Repo
+https://github.com/flovelace/hangman
+
+## Contribution
+Made with ❤️ by Fairfax
